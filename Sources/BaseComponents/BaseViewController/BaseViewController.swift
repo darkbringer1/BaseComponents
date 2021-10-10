964e03a5476c81da965736231ba31dc7efbf6354
Doğukaan Kılıçarslan <40922278+darkbringer1@users.noreply.github.com>
2021-10-10 19:36:11 +0300

    V

--- Sources/BaseComponents/BaseViewController/BaseViewController.swift
+++ Sources/BaseComponents/BaseViewController/BaseViewController.swift
@@ -4,7 +4,7 @@ import UIKit
 
 open class BaseViewController<V>: UIViewController, BaseViewControllerProtocols {
     
-    open var viewModel: V!
+    open var viewModel = ViewModel.init(viewModel: V)
     
     convenience init(viewModel: V) {
         self.init()
@@ -17,6 +17,13 @@ open class BaseViewController<V>: UIViewController, BaseViewControllerProtocols 
     }
     
     open func prepareViewControllerConfigurations() { }
+    
+}
+
+// trying viewModel var above to be accessible from outside
+struct ViewModel<V> {
+    
+    public var viewModel: V?
     
 }
 #endif
