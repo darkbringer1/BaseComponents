a41cac7b21b5017d6556d85ba4ad4e481060a7fd
Doğukaan Kılıçarslan <40922278+darkbringer1@users.noreply.github.com>
2021-10-10 18:05:54 +0300

    Dependency fixed

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BaseComponents",
-            dependencies: []),
+            dependencies: ["DefaultNetworkOperationPackage"]),
         .testTarget(
             name: "BaseComponentsTests",
             dependencies: ["BaseComponents"]),
