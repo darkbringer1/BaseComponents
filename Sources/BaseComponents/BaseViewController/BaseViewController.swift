dabb5ca9f34c866ff646e885ddb2cb7e87bbaa99
Doğukaan Kılıçarslan <40922278+darkbringer1@users.noreply.github.com>
2021-10-10 19:52:12 +0300

    Revert to 1.0.6 and viewModel made public

--- Sources/BaseComponents/BaseViewController/BaseViewController.swift
+++ Sources/BaseComponents/BaseViewController/BaseViewController.swift
@@ -4,7 +4,7 @@ import UIKit
 
 open class BaseViewController<V>: UIViewController, BaseViewControllerProtocols {
     
-    open var viewModel = ViewModel<Any>().viewModel
+    public var viewModel: V!
     
     convenience init(viewModel: V) {
         self.init()
@@ -20,10 +20,4 @@ open class BaseViewController<V>: UIViewController, BaseViewControllerProtocols 
     
 }
 
-// trying viewModel var above to be accessible from outside
-struct ViewModel<V> {
-    
-    public var viewModel: V?
-    
-}
 #endif
